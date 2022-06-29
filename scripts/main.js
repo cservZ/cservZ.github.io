ec8432485443c5478532eb1182fca9a24940b288
cservZ <cservenak99@gmail.com>
2022-06-29 21:41:44 +0200

    Responsivity and design tweaks

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,23 @@ window.onload = () => {
     const currentYear = new Date().getFullYear();
     document.querySelector('#current-year-span').innerHTML = currentYear;
 
-    // Collapse the navbar on smaller screens when a navigation link is clicked.
+    // Collapse the navbar on smaller screens when a navigation link or the navigation brand is clicked.
+
+    const closeNavbar = () => {
+        const width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+        const navbar = document.querySelector('#page-navbar');
+        const navbarToggler = document.querySelector('.navbar-toggler');
+
+        if (width <= 768 && window.getComputedStyle(navbar).display !== 'none') {
+            navbarToggler.click();
+        }
+    }
+
+    const navBrand = document.querySelector('.navbar-brand');
+    navBrand.addEventListener('click', closeNavbar);
 
     const navLinks = document.querySelectorAll('.nav-link');
     for (let navLink of navLinks) {
-        navLink.addEventListener('click', () => {
-            const width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-            if (width <= 768)                   // 768 is the official MD breakpoint in Bootstrap.
-                document.querySelector('.navbar-toggler').click();
-        });
+        navLink.addEventListener('click', closeNavbar);
     }
 };
